d304dda737460c6f5c91d2388e85706c551d9661
dengpengfei <dengpengfei@guazi.com>
2020-01-08 20:10:33 +0800

    service

--- service/http_service.dart
+++ service/http_service.dart
@@ -6,7 +6,7 @@ Future Request(url, { formData }) async {
   try {
     Response response;
     Dio dio = Dio();
-    dio.options.contentType = ContentType.parse('application/x-www-form-urlencoded');
+    dio.options.contentType = ContentType.parse('application/x-www-form-urlencoded') as String;
     response = await dio.post(url, data: formData);
     if (response.statusCode == 200) {
       return response;
